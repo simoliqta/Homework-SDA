36012f3a503d69ad25bb63cdef6ab172020e377b
simoliqta <andonov97@gmail.com>
2018-01-28 14:41:45 +0200

    Create CylceDetection.cpp

---
+++ Homework 4 and some previous tasks/CylceDetection.cpp
@@ -0,0 +1,20 @@
+bool has_cycle(Node* head) {
+
+    if (head == nullptr) { 
+        return false; 
+    }
+
+    Node* slow = head;
+    Node* fast = head;
+
+    while (fast != nullptr && fast->next != nullptr) {
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if (slow == fast) { 
+            return true; 
+        }
+    }
+
+    return false;
+}
